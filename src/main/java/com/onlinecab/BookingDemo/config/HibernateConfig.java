33fb64a1e194b2ac67a9ee0465ad74fd468a9632
LamboleSwapnil <swapnil.lambole01@gmail.com>
2020-06-13 10:39:24 +0530

    Rfactor the code.

--- src/main/java/com/onlinecab/BookingDemo/config/HibernateConfig.java
+++ src/main/java/com/onlinecab/BookingDemo/config/HibernateConfig.java
@@ -14,17 +14,6 @@ import java.util.Properties;
 @Configuration
 public class HibernateConfig {
 
-   /* @PersistenceUnit
-    private EntityManagerFactory entityManagerFactory;
-
-    @Bean
-    public SessionFactory getSessionFactory(){
-        if(entityManagerFactory.unwrap(SessionFactory.class) == null){
-            throw new NullPointerException("SessionFactory object not present");
-        }
-        return entityManagerFactory.unwrap(SessionFactory.class);
-    }*/
-
     @Value("${spring.datasource.driver-class-name}")
     private String driverClass;
     @Value("${spring.datasource.url}")
